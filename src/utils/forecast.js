eafdfffa1bdc22f61734d3c2a594bdf9a95bb305
hyl3rid <cliff.bam92@gmail.com>
2021-05-02 15:55:58 -0600

    added precipitation

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,8 +4,14 @@ const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=d63593fbbde3fb0cb2589356f65a8c6d&query=${latitude},${longitude}`;
 
   request({ url, json: true }, (error, response) => {
-     const errorMsg = response.body.error;
-     const {weather_descriptions, temperature, feelslike} = response.body.current;
+    const errorMsg = response.body.error;
+    console.log(response.body.current);
+    const {
+      weather_descriptions,
+      temperature,
+      feelslike,
+      precip,
+    } = response.body.current;
     if (error) {
       callback('Unable to connect to weather service!', undefined);
     } else if (errorMsg) {
@@ -13,7 +19,7 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `${weather_descriptions} ${temperature} ${feelslike}`
+        `${weather_descriptions}. Temperature: ${temperature}. Feels like: ${feelslike}. Precipitation: ${precip}`
       );
     }
   });
